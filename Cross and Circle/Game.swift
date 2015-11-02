51b4e3f3916a4a6db0f17c9f2a9a994aa5734618
Nicholas Smith <npsmith1990@gmail.com>
2015-11-01 20:18:53 -0600

    Planning out logic still. Gotta turn in for the night.

--- Cross and Circle/Game.swift
+++ Cross and Circle/Game.swift
@@ -9,12 +9,55 @@
 import UIKit
 
 class Game: NSObject {
+    var board = [[" "," "," "],
+                [" "," "," "],
+                [" "," "," "]]
     subscript(x:Int,y:Int)->GameState?{
         get{
             return nil
         }
-        set{
-            
+        set(letter){
+            var tic = "";
+            if String(letter!) == "Cross" {
+                tic = "X"
+                // Check to see if cross has won
+                // Start from current location [x][y]
+                // Is [x-1][y] a cross?
+                // Is [x+1][y] a cross?
+                // Is [x][y-1] a cross?
+                // Is [x][y+1] a cross?
+                
+                // Or... just check the whole graph (no edge cases)
+                // [x][y] is it a cross?
+                    // yes
+                        // [x][y+1] is it a cross?
+                            // yes
+                            // no
+                        // [x+1][y] is it a cross?
+                            // yes
+                            // no
+                        // [x+1][y+1] is it a cross?
+                            // yes
+                            // no
+                    // no
+                // [][]
+                
+                // There's gotta be a better way!!!
+            } else if String(letter!) == "Circle" {
+                tic = "O"
+                // Check to see if circle has won
+            }
+            board[y][x] = String(tic)
+            for var i = 0; i < board.count; i++ {
+                for var j = 0; j < board[i].count; j++ {
+                    print(board[i][j], terminator:"")
+                    if j == board[i].count {
+                        print("")
+                    }
+                }
+                print("")
+            }
+            print("")
         }
     }
 }
